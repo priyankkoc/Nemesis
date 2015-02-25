bbeaabff776777b31afde9a3373495bab5ae5832
Nemesis <kochhar.priyank@gmail.com>
2015-02-25 15:24:27 +0530

    my-sql script for ubuntu

---
+++ scripts/sh/mysql-server-ubuntu.sh
@@ -0,0 +1,6 @@
+#First search and install package MYSQL SERVER
+sudo apt-get install mysql-server
+echo 'Installing mysql server on the machine/server'
+#staring mysql service
+sudo service mysql start
+echo 'starting mysql service'
